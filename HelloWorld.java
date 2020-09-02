9a94dfd439f04a5a348ff71e9e65b29cf7438c38
dev <sharmadevesh33@yahoo.in>
2020-09-02 16:02:32 +0530

    ccc

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,8 @@
 import javax.swing.*;
-public class HelloWorld extends JFrame
+public class HelloWorld 
 {
 public static void main(String[] args)
 {
-new HelloWorld();
-}
-public HelloWorld()
-{
-JPanel panel1 = new JPanel();
-JLabel label1 = new JLabel("Hello, World, thiss is my first Java  program on Ubuntu Linux");
-panel1.add(label1);
-this.add(panel1);
-this.setTitle("Hello World");
-this.setSize(500,500);
-this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-this.setVisible(true);
+System.out.println("HELLO");
 }
 }
