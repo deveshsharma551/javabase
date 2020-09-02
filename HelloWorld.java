7f51212ce4cdaef744bccd8ba608e1e6981f16ad
dev <sharmadevesh33@yahoo.in>
2020-09-02 15:56:24 +0530

    check

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,7 @@ new HelloWorld();
 public HelloWorld()
 {
 JPanel panel1 = new JPanel();
-JLabel label1 = new JLabel("Hello, World, this is my first Java  program on Ubuntu Linux");
+JLabel label1 = new JLabel("Hello, World, thiss is my first Java  program on Ubuntu Linux");
 panel1.add(label1);
 this.add(panel1);
 this.setTitle("Hello World");
